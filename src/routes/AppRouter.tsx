ca793d204dbd828477d84bbfeef011bf40977d34
Giwa Toheeb <giwatoheeb27@gmail.com>
2025-08-03 15:06:42 +0100

    final cleanup

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -7,20 +7,20 @@ const Movies = lazy(() => import('../pages/Movies'))
 const Login = lazy(() => import('../pages/Login'))
 
 export default function AppRouter() {
-    return (
-      <BrowserRouter>
-        <Navbar />
-        <Suspense fallback={<p style={{ padding: 16 }}>Loading…</p>}>
-          <Routes>
-            <Route path="/" element={<Navigate to="/movies" replace />} />
-            <Route element={<ProtectedRoute />}>
-              <Route path="/movies" element={<Movies />} />
-            </Route>
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<Navigate to="/movies" replace />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Suspense fallback={<p style={{ padding: 16 }}>Loading…</p>}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/movies" replace />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/movies" element={<Movies />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/movies" replace />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  )
   }
   
